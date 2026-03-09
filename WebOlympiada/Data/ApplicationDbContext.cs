ad8ac19baeee2a13aa5c4366a617e903f2c188dc
Václav Paleček <palecv00@jcu.cz>
2026-03-09 11:06:51 +0100

    Add project files.

---
+++ WebOlympiada/Data/ApplicationDbContext.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebOlympiada.Models;
+
+namespace WebOlympiada.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+        public virtual DbSet<Sport> Sports { get; set; }
+
+        public virtual DbSet<Models.Type> Types { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Sport>(entity =>
+            {
+                entity.HasKey(e => e.Id).HasName("PK__Sports__3213E83F603D15AB");
+
+                entity.Property(e => e.Id)
+                    .ValueGeneratedNever()
+                    .HasColumnName("id");
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasColumnName("name");
+                entity.Property(e => e.TypeId).HasColumnName("TypeID");
+
+                entity.HasOne(d => d.Type).WithMany(p => p.Sports)
+                    .HasForeignKey(d => d.TypeId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("Sports.Types");
+            });
+
+            modelBuilder.Entity<Models.Type>(entity =>
+            {
+                entity.HasKey(e => e.Id).HasName("PK__Types__3214EC07AA286D7B");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50);
+            });
+
+        }
+
+
+    }
+}
---
+++ WebOlympiada/Data/Migrations/20260309093220_AddSportAndTypes.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebOlympiada.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSportAndTypes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Types",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__Types__3214EC07AA286D7B", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sports",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TypeID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__Sports__3213E83F603D15AB", x => x.id);
+                    table.ForeignKey(
+                        name: "Sports.Types",
+                        column: x => x.TypeID,
+                        principalTable: "Types",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sports_TypeID",
+                table: "Sports",
+                column: "TypeID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Sports");
+
+            migrationBuilder.DropTable(
+                name: "Types");
+        }
+    }
+}
