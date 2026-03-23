68025d036f2ce98d8bc3b875ce5777ab01b4463a
Prokýšek Miloš PhDr. Ph.D. <prokysek@jcu.cz>
2026-03-23 10:42:39 +0100

    MilosBigFix

--- WebOlympiada/Data/ApplicationDbContext.cs
+++ WebOlympiada/Data/ApplicationDbContext.cs
@@ -1,22 +1,56 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using WebOlympiada.Models;
+using Type = WebOlympiada.Models.Type;
 
 namespace WebOlympiada.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
     {
+        public virtual DbSet<Rozhodci> Rozhodci { get; set; }
+        public virtual DbSet<Divaci> Divaci { get; set; }
+
+        public virtual DbSet<Sport> Sportovec { get; set; }
         public virtual DbSet<Sport> Sports { get; set; }
 
-        public virtual DbSet<Models.Type> Types { get; set; }
+
+        public virtual DbSet<Type> Types { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Rozhodci>(entity =>
+            {
+                entity.HasKey(e => e.Id).HasName("PK__Rozhodc__3214EC07B9C8F1B7");
+                entity.ToTable("Rozhodci");
+                entity.Property(e => e.Jmeno).IsRequired();
+                entity.Property(e => e.Prijmeni).IsRequired();
+            });
+
+            modelBuilder.Entity<Divaci>(entity =>
+            {
+                entity.HasKey(e => e.Id).HasName("PK__Divaci__3214EC072BFDF6C1");
+
+                entity.ToTable("Divaci");
+
+                entity.Property(e => e.Jmeno).HasMaxLength(50);
+                entity.Property(e => e.Prijmeni).HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<Sportovec>(entity =>
+            {
+                entity.HasKey(e => e.Id).HasName("PK__Sportove__3214EC07B9C8F1B7");
+                entity.ToTable("Sportovec");
+                entity.Property(e => e.Jmeno).IsRequired();
+                entity.Property(e => e.Prijmeni).IsRequired();
+                entity.Property(e => e.Narodnost).IsRequired();
+            });
+
 
             modelBuilder.Entity<Sport>(entity =>
             {
                 entity.HasKey(e => e.Id).HasName("PK__Sports__3213E83F603D15AB");
+
+                entity.HasIndex(e => e.TypeId, "IX_Sports_TypeID");
 
                 entity.Property(e => e.Id)
                     .ValueGeneratedNever()
@@ -32,7 +66,7 @@ namespace WebOlympiada.Data
                     .HasConstraintName("Sports.Types");
             });
 
-            modelBuilder.Entity<Models.Type>(entity =>
+            modelBuilder.Entity<Type>(entity =>
             {
                 entity.HasKey(e => e.Id).HasName("PK__Types__3214EC07AA286D7B");
 
@@ -41,11 +75,10 @@ namespace WebOlympiada.Data
                     .HasMaxLength(50);
             });
 
-        }
-        public DbSet<WebOlympiada.Models.Rozhodci> Rozhodci { get; set; } = default!;
-        public DbSet<WebOlympiada.Models.Divaci> Divaci { get; set; } = default!;
-        public DbSet<WebOlympiada.Models.Sportovec> Sportovec { get; set; } = default!;
 
+            base.OnModelCreating(modelBuilder);
+        }
 
     }
 }
+
---
+++ WebOlympiada/Data/Migrations/20260323094206_MilosBigFix.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebOlympiada.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class MilosBigFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "Sports",
+                newName: "Sport");
+
+            migrationBuilder.CreateTable(
+                name: "Divaci",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Jmeno = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Prijmeni = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__Divaci__3214EC072BFDF6C1", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rozhodci",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Jmeno = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Prijmeni = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Sport = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__Rozhodc__3214EC07B9C8F1B7", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sportovec",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Jmeno = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Prijmeni = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Narodnost = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__Sportove__3214EC07B9C8F1B7", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Divaci");
+
+            migrationBuilder.DropTable(
+                name: "Rozhodci");
+
+            migrationBuilder.DropTable(
+                name: "Sportovec");
+
+            migrationBuilder.RenameTable(
+                name: "Sport",
+                newName: "Sports");
+        }
+    }
+}
